1d549a77f0a4885449b4e177f8e0fd4ebf002d83
Duclaux Tagne <tagneduclaux@gmail.com>
2025-10-26 22:55:26 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import NavBar from './components/NavBar'
+import HeroSection from './sections/HeroSection'
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import MessageSection from './sections/MessageSection';
+import FlavorSection from './sections/FlavorSection';
+
+gsap.registerPlugin(ScrollTrigger);
+
+function App() {
+  return (
+    <main>
+      <NavBar />
+      <HeroSection/>
+      <MessageSection/>
+      <FlavorSection/>
+      <div className="h-dvh border border-red-500"></div>
+    </main>
+  )
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+function NavBar() {
+  return (
+    <nav className='fixed top-0 left-0 z-50 md:p-9 p-3'>
+        <img src="/images/nav-logo.svg" alt='nav-logo' className='md:24 w-20'/>
+    </nav>
+  )
+}
+
+export default NavBar
